3795fa871938cf9685fe24840c3c4619f8f26875
Vegetable_builder <riona1209@outlook.com>
2022-05-28 13:16:05 +0530

    Yash: Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <Header v-if="$route.meta.showNavbar" />
+    <router-view />
+  </v-app>
+</template>
+
+<script>
+import Header from "./views/CommonViews/CommonComponents/Header.vue";
+export default {
+  name: "App",
+  components: { Header },
+};
+</script>
---
+++ client/src/router/index.js
@@ -0,0 +1,74 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// AUTHENTICATIONVIEWS IMPORT
+import LoginHome from "../views/AuthenticationViews/LoginHome.vue";
+// VIEWS
+import AdminDashboardVue from "../views/AdminViews/AdminDashboard.vue";
+import UserDashboardVue from "../views/UserViews/UserDashboard.vue";
+import ChangePasswordVue from "../views/CommonViews/ChangePassword.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "LoginHome",
+    component: LoginHome,
+    meta: {
+      showNavbar: false,
+    },
+  },
+  {
+    path: "/dashboard",
+    name: "UserDashboard",
+    component: UserDashboardVue,
+    meta: {
+      showNavbar: true,
+    },
+  },
+  {
+    path: "/admindashboard",
+    name: "AdminDashboard",
+    component: AdminDashboardVue,
+    meta: {
+      showNavbar: true,
+    },
+  },
+  {
+    path: "/changepassword",
+    name: "ChangePassword",
+    component: ChangePasswordVue,
+    meta: {
+      showNavbar: true,
+    },
+  },
+];
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
+
+//! Executes before going to each route
+router.beforeEach((to, from, next) => {
+  var isAuthenticated = false;
+  if (sessionStorage.getItem("user_token")) {
+    isAuthenticated = true;
+  }
+  if (isAuthenticated) {
+    if (to.name === "LoginHome") {
+      if (JSON.parse(sessionStorage.getItem("user_data")).USER_TYPE === "U") {
+        next({ name: "UserDashboard" });
+      } else {
+        next({ name: "AdminDashboard" });
+      }
+    } else {
+      next();
+    }
+  }
+  if (to.name !== "LoginHome" && !isAuthenticated) {
+    next({ name: "LoginHome" });
+  } else {
+    next();
+  }
+});
+export default router;
